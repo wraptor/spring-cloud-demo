de45876c660e0d0008b19cf60b842ebc8238ea94
seepine <seepine@163.com>
2020-03-10 18:57:34 +0800

    add nacos module

---
+++ spring-cloud-demo-03-nacos/spring-cloud-demo-03-nacos-consumer/src/main/java/com/seepine/demo/nacos/consumer/SpringCloudDemo03NacosConsumerApplication.java
@@ -0,0 +1,20 @@
+package com.seepine.demo.nacos.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringCloudDemo03NacosConsumerApplication
+ *
+ * @author seepine(seepine @ 163.com)
+ * @date 2020/3/10 14:19
+ * @since 1.1.0
+ */
+@SpringBootApplication
+public class SpringCloudDemo03NacosConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDemo03NacosConsumerApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-demo-03-nacos/spring-cloud-demo-03-nacos-provider/src/main/java/com/seepine/demo/nacos/provider/SpringCloudDemo03NacosProviderApplication.java
@@ -0,0 +1,20 @@
+package com.seepine.demo.nacos.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringCloudDemo03NacosProviderApplication
+ *
+ * @author seepine(seepine @ 163.com)
+ * @date 2020/3/10 14:09
+ * @since 1.1.0
+ */
+@SpringBootApplication
+public class SpringCloudDemo03NacosProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDemo03NacosProviderApplication.class, args);
+    }
+
+}

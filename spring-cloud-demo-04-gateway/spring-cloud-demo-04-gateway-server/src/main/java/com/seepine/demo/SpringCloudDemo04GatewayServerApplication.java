2a48cabf5075fd03a3057c9264b5a7358c932c50
seepine <seepine@163.com>
2020-03-26 11:39:47 +0800

    add gateway and open gateway discovery

--- spring-cloud-demo-04-gateway/spring-cloud-demo-04-gateway-server/src/main/java/com/seepine/demo/SpringCloudDemo04GatewayServerApplication.java
+++ spring-cloud-demo-04-gateway/spring-cloud-demo-04-gateway-server/src/main/java/com/seepine/demo/SpringCloudDemo04GatewayServerApplication.java
@@ -3,11 +3,15 @@ package com.seepine.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Seepine
+ * @date 2020/3/26 10:23
+ */
 @SpringBootApplication
-public class DemoApplication {
+public class SpringCloudDemo04GatewayServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SpringCloudDemo04GatewayServerApplication.class, args);
     }
 
 }
